c73edd24ccab8c7dc50bf665a2d996ab93632630
John <jherrma6@jhu.edu>
2018-12-27 14:21:23 -0600

    update of main with test of url_reader copy function

--- Web_Scraping/src/web_scraping/Web_Scraping.java
+++ Web_Scraping/src/web_scraping/Web_Scraping.java
@@ -9,23 +9,33 @@ package web_scraping;
 import web_reader.*;
 import indeed.*;
 
+import file_io.*;
+
 public class Web_Scraping 
 {
 
     public static void main(String[] args) 
     {
         String url = "https://www.indeed.com/jobs?q=electrical+engineer&l=Round+Lake%2C+IL";
-        url_reader file = new url_reader(url);
-        file.open();
-        String line = file.readLine();
-        indeed indeed_search = new indeed();
+        url_reader file1 = new url_reader(url);
+        file1.open(1024);
         
-        while(line != null)
+        url_reader file2 = new url_reader(url);
+        file2.copy(file1);
+        
+        
+        
+        String lineA = file1.readLine();
+        String lineB = file2.readLine();
+        //indeed indeed_search = new indeed();
+        
+        while(lineA != null | lineB != null)
         {
-            //System.out.println(indeed_search.numJobs(line));
-            indeed_search.find_a_job(line);
-            line = file.readLine();
+            System.out.println(lineA);
+            System.out.println(lineB);
+            
+            lineA = file1.readLine();
+            lineB = file2.readLine();
         }
-        file.close();
     }
 }
