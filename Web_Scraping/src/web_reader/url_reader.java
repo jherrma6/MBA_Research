2918057713fb240ab9e780fdd6722bd7312ceed5
John <jherrma6@jhu.edu>
2018-12-27 14:21:47 -0600

    update of url_reader with new features such as copy() method

--- Web_Scraping/src/web_reader/url_reader.java
+++ Web_Scraping/src/web_reader/url_reader.java
@@ -5,15 +5,19 @@
  * Date       : December 20th, 2018
  ******************************************************************************/
 package web_reader;
+import file_io.string_buffer;
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
 
 
 public class url_reader 
 {
-    public String         url_string;                          // website URL
-    public URL            url_stream;                          // URL Stream
-    public BufferedReader url_buffer;                          // Buffer
+    private String         url_string;                          // website URL
+    private URL            url_stream;                          // URL Stream
+    private BufferedReader url_buffer;                          // Buffer
+    private string_buffer  url_contents;                        // Contents of the url
+    private int            buffer_size;
     
  /******************************************************************************
  * Constructor
@@ -25,82 +29,132 @@ public class url_reader
  ******************************************************************************/ 
     public url_reader(String url_string)
     {
-        this.url_string = url_string;
-        this.url_stream = null;
-        this.url_buffer = null;
+        this.url_string   = url_string;
+        this.url_stream   = null;
+        this.url_buffer   = null;
+        this.url_contents = null;
+        this.buffer_size  = 1000*1024;
     }
  /******************************************************************************
  * Method : open()
  * Purpose: Opens a stream containing the URL's HTML. Returns a BufferedReader
  *          reference that can be used to access the stream. 
  * 
- * @return BufferedReader object
+ * @param  size
  * @author johnherrmann
  ******************************************************************************/   
-    public BufferedReader open()
+    public void open(int size)
     {
-        if(this.url_string == null)
+        if(this.url_string == null)             //check if url does not exist
         {
-            return null;
+            return;
         }
-        try
+        try                                     //Attempt to open url and 
+                                                //... transfer contents to 
+                                                //... string buffer
         {
-            int numKB = 1000;
-            int bufferSize = 1024 *numKB;
+            this.buffer_size = 1024 * size;       //Set the  BufferedReader size
             this.url_stream = new URL(this.url_string);
             this.url_buffer = new BufferedReader(
                                 new InputStreamReader(url_stream.openStream()),
-                                bufferSize);
-            return this.url_buffer;
+                                this.buffer_size);
+                                                //Transfer contents of URL
+                                                //...to the string_buffer obj
+            this.url_contents = new string_buffer(this.url_buffer);
+            
+            try                                 //Close the BufferedReader
+            {
+                this.url_buffer.close();
+                return;
+            }
+            catch (Exception e)
+            {
+            return;
+            }
         }
         catch (Exception e)
         {
-            return null;
+            return;
         }
     }
 /******************************************************************************
  * Method       : readLine()
- * Preconditions: A valid URL must be "opened" using the open() method. 
- * Purpose      : attempts to read a line from the BufferedReader object. 
+ * Preconditions: (1) A valid URL must be "opened" using the open() method. 
+ *                (2) Contents must be transferred to string buffer
+ * Purpose      : attempts to read a line from the string_buffer object. 
  * 
- * @param  none
  * @return String
  * @author johnherrmann
  ******************************************************************************/
   public String readLine()
   {
-    if(this.url_buffer == null)
-    {
-        return null;
-    }
-    try
-        {
-           return this.url_buffer.readLine();
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
+      if(this.url_contents == null)
+      {
+          return null;
+      }
+      else
+      {
+          return this.url_contents.readLine();
+      }
+      
+  }
+/******************************************************************************
+ * Method       : get_url_string();
+ * Purpose      : returns the current url_string
+ * 
+ * @return      : url_string
+ ******************************************************************************/
+ public String get_url_string()
+ {
+     return this.url_string;
+ }
+ /******************************************************************************
+ * Method       : get_url_stream();
+ * Purpose      : returns the current url_stream
+ * 
+ * @return      : url_stream
+ ******************************************************************************/
+ public URL get_url_stream()
+ {
+     return this.url_stream;
+ }
+ /******************************************************************************
+ * Method       : get_url_buffer();
+ * Purpose      : returns the current url_buffer
+ * 
+ * @return      : url_buffer
+ ******************************************************************************/
+ public BufferedReader get_url_buffer()
+ {
+     return this.url_buffer;
+ }
+ /******************************************************************************
+ * Method       : get_url_contents();
+ * Purpose      : returns the current url_contents
+ * 
+ * @return      : url_contents
+ ******************************************************************************/
+ public string_buffer get_url_contents()
+ {
+     return this.url_contents;
  }
 /******************************************************************************
- * Method        : close()
- * Preconditions : A buffer has been opened.
- * Purpose       : Attempts to read a line from the BufferedReader object.
- * Postconditions: The buffer has been closed. 
+ * Method       : copy()
+ * Preconditions: none
+ *                
+ * Purpose      : creates a copy of the object
+ * 
+ * @return String
+ * @author johnherrmann
  ******************************************************************************/
-public void close()
-{
-    if(this.url_buffer == null)
-    {
-        return;
-    }
-    try
-        {
-          this.url_buffer.close();
-        }
-        catch (Exception e)
-        {
-            return;
-        }   
-}
+ public void copy(url_reader source)
+ {
+        this.url_string    = source.get_url_string();
+        this.url_stream    = source.get_url_stream();
+        this.url_buffer    = source.get_url_buffer();
+        string_buffer temp = new string_buffer(null);
+        temp.buffer = (ArrayList<String>)source.get_url_contents().buffer.clone();
+        temp.buffer_size = source.get_url_contents().buffer_size;
+        this.url_contents = temp;
+ }
 }
